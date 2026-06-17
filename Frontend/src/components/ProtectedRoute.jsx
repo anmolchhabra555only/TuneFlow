1e53f2bc0fac7763cd984b0106efe76be73cf606
Anmol Chhabra <pintu9999671@gmail.com>
2026-06-17 23:41:48 +0530

    Fixed route for Deployment

--- Frontend/src/components/ProtectedRoute.jsx
+++ Frontend/src/components/ProtectedRoute.jsx
@@ -2,7 +2,11 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
 
+  console.log("Cookies:", document.cookie);
+
   const isLoggedIn = document.cookie.includes("token");
+
+  console.log("isLoggedIn:", isLoggedIn);
 
   return isLoggedIn
     ? children
