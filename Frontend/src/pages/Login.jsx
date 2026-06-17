f9bef98bea48cb032bbab96385f41b46c545c828
Anmol Chhabra <pintu9999671@gmail.com>
2026-06-17 23:18:56 +0530

    Fixed production login API

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -16,7 +16,7 @@ const Login = () => {
     try {
 
       const response = await axios.post(
-        "http://localhost:3000/api/auth/login",
+        "https://tuneflow-qgbu.onrender.com/api/auth/login",
         {
           email,
           password
